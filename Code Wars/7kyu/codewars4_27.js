ef06312d912af647dd9d70f31f9cbc29cbb0fa6e
alcarreon <alcarreon20@gmail.com>
2022-04-27 20:53:32 -0400

    additional codewars problems

--- Code Wars/7kyu/codewars4_27.js
+++ Code Wars/7kyu/codewars4_27.js
@@ -10,4 +10,23 @@ let isIsogram = (str) => {
 // console.log(isIsogram("isIsogram")); // false
 // console.log(isIsogram(""));
 
-console.log((s = "aaaxbbbbyyhwawiwjjjwwm".length));
+// Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+// The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+// Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+
+let calculateYears = (principal, interest, tax, desired) => {
+  if (principal === desired) {
+    return 0;
+  } else {
+    for (y = 0; principal <= desired; y++) {
+      principal += principal * interest - principal * interest * tax;
+    }
+    return y;
+  }
+};
+
+console.log(calculateYears(1000, 0.05, 0.18, 1100)); // 3
+console.log(calculateYears(1000, 0.01625, 0.18, 1200)); // 14
+console.log(calculateYears(1000, 0.05, 0.18, 1000)); // 0
