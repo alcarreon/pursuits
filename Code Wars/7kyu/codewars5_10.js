8218f2245b3e40cfb51b5b4141d38c24d4f74000
alcarreon <alcarreon20@gmail.com>
2022-05-10 22:59:58 -0400

    additional code wars challenegs

--- Code Wars/7kyu/codewars5_10.js
+++ Code Wars/7kyu/codewars5_10.js
@@ -22,5 +22,29 @@ let SeriesSum = (n) => {
 
 let number = (arr) => arr.map((x, y) => `${y + 1}: ${x}`);
 
-console.log(number([])); // []
-console.log(number(["a", "b", "c"])); // ["1: a", "2: b", "3: c"]
+// console.log(number([])); // []
+// console.log(number(["a", "b", "c"])); // ["1: a", "2: b", "3: c"]
+
+// In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+// Examples:
+
+// 1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+// 4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+
+let dontGiveMeFive = (s, e) => {
+  for (cnt = 0; s <= e; ++s) {
+    (s + "").includes("5") || cnt++;
+  }
+  return cnt;
+};
+
+// let dontGiveMeFive = (num1, num2) => {
+//     starter = []
+//     for(num1; num1 <= num2; num1++){
+//         num1 % 5 !== 0 ? starter.push(num1) : starter
+//     } return starter.length
+// }
+console.log;
+// console.log(dontGiveMeFive(1, 9)); // 8
+console.log(dontGiveMeFive(4, 17)); // 12
