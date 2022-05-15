8c4074507a92048cddb202c602ccbfd907a74d3d
alcarreon <alcarreon20@gmail.com>
2022-05-14 20:17:23 -0400

    additional codewars challenges

--- Code Wars/7kyu/codewars5_14.js
+++ Code Wars/7kyu/codewars5_14.js
@@ -81,7 +81,64 @@ const arithmetic = (num1, num2, operator) => {
   }
 };
 
-console.log(arithmetic(1, 2, "add")); // 3
-console.log(arithmetic(8, 2, "subtract")); // 6
-console.log(arithmetic(5, 2, "multiply")); // 10
-console.log(arithmetic(8, 2, "divide")); // 4
+// console.log(arithmetic(1, 2, "add")); // 3
+// console.log(arithmetic(8, 2, "subtract")); // 6
+// console.log(arithmetic(5, 2, "multiply")); // 10
+// console.log(arithmetic(8, 2, "divide")); // 4
+
+// Your task is to make function, which returns the sum of a sequence of integers.
+
+// The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+// If begin value is greater than the end, function should returns 0
+
+// Examples
+// 2,2,2 --> 2
+// 2,6,2 --> 12 (2 + 4 + 6)
+// 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+// 1,5,3  --> 5 (1 + 4)
+
+// parameters three numbers, starting, end, step(inclusive)
+// returns single value the sum of the numbers between starting and end moving by step
+// examples
+// psuedo code take start number and compare it to the end number if it is lower add step until it is bigger than end number
+
+const sequenceSum = (start, end, step) => {
+  let stepped = 0;
+  for (start; start <= end; start += step) {
+    stepped += start;
+  }
+  return stepped;
+};
+
+// console.log(sequenceSum(2, 6, 2)); // 12
+// console.log(sequenceSum(1, 5, 1)); // 15
+// console.log(sequenceSum(1, 5, 3)); // 5
+
+// Your task is to write function findSum.
+
+// Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+// For example:
+
+// findSum(5) should return 8 (3 + 5)
+
+// findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+// parameters a number that is the upper limit
+// returns sum of all multiples of 3 and 5
+// examples
+// pseudo code loop through the number once with multiples of 3 loop through again with multiples of 5 add them together
+
+const findSum = (num) => {
+  let counter = [];
+  for (let i = 0; i <= num; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      counter.push(i);
+    }
+  }
+  return counter.reduce((x, y) => x + y);
+};
+
+console.log(findSum(5)); // 8
+console.log(findSum(10)); // 33
