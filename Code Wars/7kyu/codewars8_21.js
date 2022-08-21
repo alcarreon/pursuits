7e73a58960622c907e5099b2e89043508b9f4aaf
alcarreon <alcarreon20@gmail.com>
2022-08-21 18:15:41 -0400

    codewars challenge for 8_21

--- Code Wars/7kyu/codewars8_21.js
+++ Code Wars/7kyu/codewars8_21.js
@@ -15,7 +15,7 @@
 const incrementer = (arr) =>
   arr.map((x, y) => (x + y + 1 >= 10 ? (x + y + 1) % 10 : x + y + 1));
 
-console.log(incrementer([1, 2, 3])); // [2, 4, 6]
-console.log(incrementer([4, 6, 7, 1, 3])); // [5, 8, 0, 5, 8]
-console.log(incrementer([3, 6, 9, 8, 9])); // [4, 8, 2, 2, 4]
-console.log(incrementer([1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 8])); // [2, 4, 6, 8, 0, 2, 4, 6, 8, 9, 0, 1, 2, 2]
+// console.log(incrementer([1, 2, 3])); // [2, 4, 6]
+// console.log(incrementer([4, 6, 7, 1, 3])); // [5, 8, 0, 5, 8]
+// console.log(incrementer([3, 6, 9, 8, 9])); // [4, 8, 2, 2, 4]
+// console.log(incrementer([1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9, 8])); // [2, 4, 6, 8, 0, 2, 4, 6, 8, 9, 0, 1, 2, 2]
