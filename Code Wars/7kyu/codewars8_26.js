aaa86e3229d9235d68561235d4b6f9af262ad456
alcarreon <alcarreon20@gmail.com>
2022-08-29 20:37:32 -0400

    added prep

--- Code Wars/7kyu/codewars8_26.js
+++ Code Wars/7kyu/codewars8_26.js
@@ -1,5 +1,10 @@
 // Reversed Words
 
+// parameters string
+// return string
+// examples
+// pseudo code given a string split it by spaces and then use reverse on the array join the array back into a string
+
 const reverseWords = (str) => str.split(" ").reverse().join(" ");
 
 console.log(reverseWords("hello world!")); // "world! hello"
