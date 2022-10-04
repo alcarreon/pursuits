7fdf7892b0da110e6111a7dcd146718fe783986b
alcarreon <alcarreon20@gmail.com>
2022-10-04 18:40:59 -0400

    codewars challenge for 10_4

---
+++ Code Wars/6kyu/codewars10_4.js
@@ -0,0 +1,26 @@
+// Find Screen Size
+
+// Cheesy Cheeseman just got a new monitor! He is happy with it, but he just discovered that his old desktop wallpaper no longer fits. He wants to find a new wallpaper, but does not know which size wallpaper he should be looking for, and alas, he just threw out the new monitor's box. Luckily he remembers the width and the aspect ratio of the monitor from when Bob Mortimer sold it to him. Can you help Cheesy out?
+
+// The Challenge
+// Given an integer width and a string ratio written as WIDTH:HEIGHT, output the screen dimensions as a string written as WIDTHxHEIGHT.
+
+// Note: The calculated height should be represented as an integer. If the height is fractional, truncate it.
+
+// parameters number, string
+// return string
+// examples
+// pseudo code given a number and string split the string by :, create a new variable to hold the ratio by taking the first element from the split string divided by the second element of the split string, find the height by taking the width divided by the height, use a string literal to output the result
+
+const findScreenHeight = (width, stringRatio) => {
+  let splitRatio = stringRatio.split(":");
+  let ratio = splitRatio[0] / splitRatio[1];
+  let height = width / ratio;
+  return `${width}x${height}`;
+};
+
+console.log(findScreenHeight(1024, "4:3")); // "1024x768"
+
+// console.log(findScreenHeight(1280, "16:9")); // "1280x720"
+
+// console.log(findScreenHeight(3840, "32:9")); // "3840x1080"
