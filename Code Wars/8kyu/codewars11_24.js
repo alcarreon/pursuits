8f0caba0dc482a7d07bf107e5b7146b18985d83f
alcarreon <alcarreon20@gmail.com>
2022-11-23 01:09:10 -0500

    codewars challenge for 11/24

---
+++ Code Wars/8kyu/codewars11_24.js
@@ -0,0 +1,16 @@
+// Type of sum
+
+// parameters two characters
+// return string
+// examples
+// pseudo code given two characters add them together then use typeof to find their type
+
+function typeOfSum(a, b) {
+  return typeof (a + b);
+}
+
+console.log(typeOfSum(true, 1)); //'number'
+console.log(typeOfSum("s", false)); //'string'
+console.log(typeOfSum(null, undefined)); //'number'
+console.log(typeOfSum(undefined, true)); //'number'
+console.log(typeOfSum(null, false)); //'number'
