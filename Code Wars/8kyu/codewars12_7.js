e5c5cccfeaa7e8c8685995913e8c4dadb9a15cac
alcarreon <alcarreon20@gmail.com>
2022-12-07 20:10:10 -0500

    codewars challenge for 12/7

---
+++ Code Wars/8kyu/codewars12_7.js
@@ -0,0 +1,66 @@
+// Welcome!
+
+// Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+// The Task
+// Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+// Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+// The Database
+// english: 'Welcome',
+// czech: 'Vitejte',
+// danish: 'Velkomst',
+// dutch: 'Welkom',
+// estonian: 'Tere tulemast',
+// finnish: 'Tervetuloa',
+// flemish: 'Welgekomen',
+// french: 'Bienvenue',
+// german: 'Willkommen',
+// irish: 'Failte',
+// italian: 'Benvenuto',
+// latvian: 'Gaidits',
+// lithuanian: 'Laukiamas',
+// polish: 'Witamy',
+// spanish: 'Bienvenido',
+// swedish: 'Valkommen',
+// welsh: 'Croeso'
+// Possible invalid inputs include:
+
+// IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
+// IP_ADDRESS_NOT_FOUND - ip address not in the database
+// IP_ADDRESS_REQUIRED - no ip address was supplied
+
+// parameters string
+// return string
+// examples
+// pseudo code create an object with the key as the language and the value as the welcome message, check to see that the string is in the object if not return welcome otherise use bracket notation to retrieve the correct welcome in langague object
+
+function greet(str) {
+  let languages = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+  if (!languages[str]) {
+    return "Welcome";
+  } else {
+    return languages[str];
+  }
+}
+
+console.log(greet("english")); // 'Welcome'
+console.log(greet("dutch")); // 'Welkom'
+console.log(greet("IP_ADDRESS_INVALID")); // 'Welcome'
