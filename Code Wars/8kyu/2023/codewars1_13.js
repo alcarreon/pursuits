054ba9565124c288e829e689b750dabbfc629325
alcarreon <alcarreon20@gmail.com>
2023-01-13 12:45:08 -0500

    codewars challenge for 1/13

--- Code Wars/8kyu/2023/codewars1_13.js
+++ Code Wars/8kyu/2023/codewars1_13.js
@@ -32,3 +32,5 @@ function pipeFix(arr) {
 console.log(pipeFix([1, 2, 3, 5, 6, 8, 9])); // [1,2,3,4,5,6,7,8,9]
 
 console.log(pipeFix([1, 2, 3, 12])); // [1,2,3,4,5,6,7,8,9,10,11,12]
+
+console.log(pipeFix([1, 5])); // [ 1, 2, 3, 4, 5 ]
