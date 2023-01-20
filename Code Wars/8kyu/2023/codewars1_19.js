5d00d4b1b63ef5e60037f2132592814d579567bc
alcarreon <alcarreon20@gmail.com>
2023-01-19 19:19:19 -0500

    codewars challenge for 1/19

---
+++ Code Wars/8kyu/2023/codewars1_19.js
@@ -0,0 +1,27 @@
+// How many lightsabers do you own?
+
+// Inspired by the development team at Vooza, write the function that
+
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+// Note: your function should have a default parameter.
+
+// For example(Input --> Output):
+
+// "anyone else" --> 0
+// "Zach" --> 18
+
+// parameters string
+// return integer
+// examples
+// pseudo code given a string check to see that it equal Zach
+
+function howManyLightsabersDoYouOwn(name) {
+  return name == "Zach" ? 18 : 0;
+}
+
+console.log(howManyLightsabersDoYouOwn()); // 0
+console.log(howManyLightsabersDoYouOwn("Adam")); // 0
+console.log(howManyLightsabersDoYouOwn("Zach")); // 18
