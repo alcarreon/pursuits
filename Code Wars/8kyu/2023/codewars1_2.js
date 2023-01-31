8937762786d39812ea6ed3027682dbb3ca7eda13
alcarreon <alcarreon20@gmail.com>
2023-01-31 12:20:12 -0500

    codewars challenge for 1/2

---
+++ Code Wars/8kyu/2023/codewars1_2.js
@@ -0,0 +1,33 @@
+// Grasshopper - Check for factor
+
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+// About factors
+// Factors are numbers you can multiply together to get another number.
+
+// 2 and 3 are factors of 6 because: 2 * 3 = 6
+
+// You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+// You can use the mod operator (%) in most languages to check for a remainder
+// For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+// Note: base is a non-negative number, factor is a positive number.
+
+// parameters two integers
+// return boolean
+// examples
+// pseudo code check to see that the mod of the numbers is zero
+
+function checkForFactor(base, factor) {
+  return base % factor == 0;
+}
+
+console.log(checkForFactor(10, 2)); // true
+
+console.log(checkForFactor(63, 7)); // true
+
+console.log(checkForFactor(9, 2)); // false
+
+console.log(checkForFactor(653, 7)); // false
