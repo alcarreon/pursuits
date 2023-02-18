6fc3193aabfe5ce5bfc295f5e21608b8d9386296
alcarreon <alcarreon20@gmail.com>
2023-02-17 20:21:21 -0500

    codewars challenge for 2/17

--- Code Wars/8kyu/2023/codewars2_17.js
+++ Code Wars/8kyu/2023/codewars2_17.js
@@ -9,8 +9,31 @@
 // You may use any valid syntax you like; however, it is highly recommended that you complete this Kata using ES6 syntax and features.
 
 class Person {
-  constructor(firstName = "John") {}
+  constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+  }
+  sayFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  static greetExtraTerrestrials(space) {
+    return `Welcome to Planet Earth ${space}`;
+  }
 }
 
 let john = new Person();
-console.log(john.firstName);
+// console.log(john.firstName); // "John"
+// console.log(john.lastName); // "Doe"
+// console.log(john.age); // 0
+// console.log(john.gender); // "Male"
+// console.log(john.sayFullName()); // "John Doe"
+
+let jane = new Person("Jane", "Doe", 25, "Female");
+console.log(jane.firstName); // "John"
+console.log(jane.lastName); // "Doe"
+console.log(jane.age); // 0
+console.log(jane.gender); // "Male"
+console.log(jane.sayFullName()); // "John Doe"
+console.log(Person.greetExtraTerrestrials("Martians"));
