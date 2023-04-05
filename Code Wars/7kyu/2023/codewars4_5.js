442c6e134841dc8f029c68520f872292e73ddd1f
alcarreon <alcarreon20@gmail.com>
2023-04-05 19:21:21 -0400

    codewars challenge for 4/5

---
+++ Code Wars/7kyu/2023/codewars4_5.js
@@ -0,0 +1,25 @@
+// Discover The Original Price
+
+// We need to write some code to return the original price of a product, the return type must be of type decimal and the number must be rounded to two decimal places.
+
+// We will be given the sale price (discounted price), and the sale percentage, our job is to figure out the original price.
+
+// For example:
+// Given an item at $75 sale price after applying a 25% discount, the function should return the original price of that item before applying the sale percentage, which is ($100.00) of course, rounded to two decimal places.
+
+// DiscoverOriginalPrice(75, 25) => 100.00M where 75 is the sale price (discounted price), 25 is the sale percentage and 100 is the original price
+
+// parameters two intgers
+// return integer
+// examples
+// pseudo code divide the discount the produce of 1 - converting the sale price to a decimal then fix the decimal places to 2
+
+function discoverOriginalPrice(discount, saleperc) {
+  return +(discount / (1 - saleperc / 100)).toFixed(2);
+}
+
+console.log(discoverOriginalPrice(75, 25)); // 100
+
+console.log(discoverOriginalPrice(25, 75)); // 100
+
+console.log(discoverOriginalPrice(75.75, 25)); // 101
