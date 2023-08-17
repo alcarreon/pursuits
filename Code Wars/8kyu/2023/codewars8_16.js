9c74bebd5f0c9dc9992cf913458f9ac0d7f2534c
alcarreon <alcarreon20@gmail.com>
2023-08-16 20:19:20 -0400

    codewars challenge for 8/16

---
+++ Code Wars/8kyu/2023/codewars8_16.js
@@ -0,0 +1,17 @@
+// Merging sorted integer arrays (without duplicates)
+
+// Write a function that merges two sorted arrays into a single one. The arrays only contain integers. Also, the final outcome must be sorted and not have any duplicate.
+
+// parameters two arrays
+// returns sorted array
+// examples
+// psuedo code combine the two arrays then use set to remove any duplicates, convert the set object into an array and then sort the array
+
+function mergeArrays(arr1, arr2) {
+  let sortedArr = Array.from(new Set(arr1.concat(arr2))).sort((x, y) => x - y);
+  return sortedArr;
+}
+
+console.log(mergeArrays([1, 3, 5], [2, 4, 6])); // [1, 2, 3, 4, 5, 6]
+
+console.log(mergeArrays([2, 4, 8], [2, 4, 6])); // [2, 4, 6, 8]
